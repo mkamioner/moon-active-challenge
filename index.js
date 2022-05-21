15fa37bfc891a142b17112712a5708a5bf535d31
Mo Kamioner <mo@gamitee.com>
2022-05-22 00:17:12 +0300

    hmmm

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 const fs = require('fs');
 const http = require('http');
+const redis = require('redis');
 
 const port = +process.argv[2] || 3000;
 
-const client = require('redis').createClient();
+// const client = require('redis').createClient();
 
-client.on('error', (err) => console.log('Redis Client Error', err));
+const DATABASE_CONNECTIONS = ['01234567', '89abcdef']
+  .reduce((prev, curr, index) => {
+    const clients = [...curr].reduce((allClients, shard) => {
+      const client = redis.createClient({ database: index });
+      client.on('error', (err) => console.log('Redis Client Error', index, err));
+      return { ...allClients, [shard]: client };
+    }, {});
+
+    return {
+      ...prev,
+      ...clients,
+    };
+  }, {});
+
 const cardsData = fs.readFileSync('./cards.json');
 const cards = JSON.parse(cardsData);
 const CARD_DATA = cards.reduce(
@@ -15,7 +29,7 @@ const CARD_DATA = cards.reduce(
 const DATA_COMPLETE = JSON.stringify({ id: 'ALL CARDS' });
 const DATA_COMPLETE_LENGTH = DATA_COMPLETE.length;
 async function getMissingCard(key) {
-  const newIndex = (await client.incr(key)) - 1;
+  const newIndex = (await DATABASE_CONNECTIONS[(key || 'a')[0]].incr(key)) - 1;
   if (newIndex < 100) {
     return [CARD_DATA[newIndex], 91];
   }
@@ -31,10 +45,14 @@ const server = http.createServer((req, res) => {
   }).catch((err) => console.log(err));
 });
 
-client.on('ready', () => {
+Promise.all(
+  Object.keys(DATABASE_CONNECTIONS)
+    .map((shard) => new Promise((resolve) => {
+      DATABASE_CONNECTIONS[shard].on('ready', resolve);
+      DATABASE_CONNECTIONS[shard].connect();
+    })),
+).then(() => {
   server.listen(port, '0.0.0.0', () => {
     console.log(`Example app listening at http://0.0.0.0:${port}`);
   });
 });
-
-client.connect();
