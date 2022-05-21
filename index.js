58585398ee4a046dded37e54f55eb7dfc9ec23b6
Mo Kamioner <mo@gamitee.com>
2022-05-21 22:33:47 +0300

    launch from one thread

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const http = require('http');
 const cluster = require('cluster');
 
-const port = +process.argv[2] || 3000;
+const PORT = +process.argv[2] || 3000;
 const THREADS = +process.env.THREAD_COUNT || 1;
 
 const client = require('redis').createClient();
@@ -25,20 +25,25 @@ async function getMissingCard(key) {
 }
 const CONTENT_TYPE = 'application/json; charset=utf-8';
 
-const server = http.createServer((req, res) => {
-  if (req.url[1] !== 'r') {
-    getMissingCard(req.url.substring(13)).then(([body, length]) => {
-      res.writeHead(200, { 'Content-Type': CONTENT_TYPE, 'Content-Length': length });
-      res.write(body);
-      res.end();
-    }).catch((err) => console.log(err));
-    return;
-  }
+function initServer(port) {
+  const server = http.createServer((req, res) => {
+    if (req.url[1] !== 'r') {
+      getMissingCard(req.url.substring(13)).then(([body, length]) => {
+        res.writeHead(200, { 'Content-Type': CONTENT_TYPE, 'Content-Length': length });
+        res.write(body);
+        res.end();
+      }).catch((err) => console.log(err));
+      return;
+    }
 
-  res.writeHead(200, { 'Content-Type': CONTENT_TYPE, 'Content-Length': 14 });
-  res.write('{"ready":true}');
-  res.end();
-});
+    res.writeHead(200, { 'Content-Type': CONTENT_TYPE, 'Content-Length': 14 });
+    res.write('{"ready":true}');
+    res.end();
+  });
+  server.listen(port, '0.0.0.0', () => {
+    console.log(`Example app listening at http://0.0.0.0:${port}`);
+  });
+}
 
 if (THREADS > 1 && cluster.isPrimary) {
   for (let i = 0; i < THREADS; i += 1) {
@@ -46,9 +51,10 @@ if (THREADS > 1 && cluster.isPrimary) {
   }
 } else {
   client.on('ready', () => {
-    server.listen(port, '0.0.0.0', () => {
-      console.log(`Example app listening at http://0.0.0.0:${port}`);
-    });
+    if (PORT === 4001) {
+      initServer(4001);
+      initServer(4002);
+    }
   });
 
   client.connect();
