c1d0e6155f0f236f27606436c15788b60d87da9e
Mo Kamioner <mo@gamitee.com>
2022-05-21 23:26:59 +0300

    foo

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const fs = require('fs');
 const http = require('http');
-const cluster = require('cluster');
 
 const port = +process.argv[2] || 3000;
-const THREADS = +process.env.THREAD_COUNT || 1;
 
 const client = require('redis').createClient();
 
@@ -26,32 +24,17 @@ async function getMissingCard(key) {
 const CONTENT_TYPE = 'application/json; charset=utf-8';
 
 const server = http.createServer((req, res) => {
-  if (req.url[1] !== 'r') {
-    getMissingCard(req.url.substring(13)).then(([body, length]) => {
-      res.writeHead(200, { 'Content-Type': CONTENT_TYPE, 'Content-Length': length });
-      res.write(body);
-      res.end();
-    }).catch((err) => console.log(err));
-    return;
-  }
-
-  res.writeHead(200, { 'Content-Type': CONTENT_TYPE, 'Content-Length': 14 });
-  res.write('{"ready":true}');
-  res.end();
+  getMissingCard(req.url.substring(13)).then(([body, length]) => {
+    res.writeHead(200, { 'Content-Type': CONTENT_TYPE, 'Content-Length': length });
+    res.write(body);
+    res.end();
+  }).catch((err) => console.log(err));
 });
 
-if (THREADS > 1 && cluster.isPrimary) {
-  for (let i = 0; i < THREADS; i += 1) {
-    cluster.fork();
-  }
-} else {
-  client.on('ready', () => {
-    server.listen(port, '0.0.0.0', () => {
-      console.log(`Example app listening at http://0.0.0.0:${port}`);
-    });
+client.on('ready', () => {
+  server.listen(port, '0.0.0.0', () => {
+    console.log(`Example app listening at http://0.0.0.0:${port}`);
   });
+});
 
-  client.connect();
-}
-// forking
-// minimize
+client.connect();
