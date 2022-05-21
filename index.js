0e7452aab243827f47823812b0cb257547f6bb34
Mo Kamioner <mo@gamitee.com>
2022-05-21 22:45:54 +0300

    optimizations

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-const fs = require('fs');
 const http = require('http');
 const cluster = require('cluster');
+const cards = require('./cards.json');
 
 const PORT = +process.argv[2] || 3000;
 const THREADS = +process.env.THREAD_COUNT || 1;
 
-const cardsData = fs.readFileSync('./cards.json');
-const cards = JSON.parse(cardsData);
 const CARD_DATA = cards.reduce(
   (prev, curr, index) => ({ ...prev, [index]: JSON.stringify(curr) }),
   {},
@@ -14,6 +12,8 @@ const CARD_DATA = cards.reduce(
 const DATA_COMPLETE = JSON.stringify({ id: 'ALL CARDS' });
 const DATA_COMPLETE_LENGTH = DATA_COMPLETE.length;
 const USER_DATA = {};
+const CONTENT_TYPE = 'application/json; charset=utf-8';
+
 async function getMissingCard(key) {
   const newIndex = (USER_DATA[key] ?? 0) + 1;
   USER_DATA[key] = newIndex;
@@ -22,22 +22,37 @@ async function getMissingCard(key) {
   }
   return [DATA_COMPLETE, DATA_COMPLETE_LENGTH];
 }
-const CONTENT_TYPE = 'application/json; charset=utf-8';
+let api;
+function returnCardApi(req, res) {
+  getMissingCard(req.url.substring(13)).then(([body, length]) => {
+    res.writeHead(200, { 'Content-Type': CONTENT_TYPE, 'Content-Length': length });
+    res.write(body);
+    res.end();
+  }).catch((err) => console.log(err));
+}
+function returnIsUpApi(req, res) {
+  api = returnCardApi;
+  res.writeHead(200, { 'Content-Type': CONTENT_TYPE, 'Content-Length': 14 });
+  res.write('{"ready":true}');
+  res.end();
+}
 
+api = returnIsUpApi;
 function initServer(port) {
   const server = http.createServer((req, res) => {
-    if (req.url[1] !== 'r') {
-      getMissingCard(req.url.substring(13)).then(([body, length]) => {
-        res.writeHead(200, { 'Content-Type': CONTENT_TYPE, 'Content-Length': length });
-        res.write(body);
-        res.end();
-      }).catch((err) => console.log(err));
-      return;
-    }
+    api(req, res);
+    // if (req.url[1] !== 'r') {
+    //   getMissingCard(req.url.substring(13)).then(([body, length]) => {
+    //     res.writeHead(200, { 'Content-Type': CONTENT_TYPE, 'Content-Length': length });
+    //     res.write(body);
+    //     res.end();
+    //   }).catch((err) => console.log(err));
+    //   return;
+    // }
 
-    res.writeHead(200, { 'Content-Type': CONTENT_TYPE, 'Content-Length': 14 });
-    res.write('{"ready":true}');
-    res.end();
+    // res.writeHead(200, { 'Content-Type': CONTENT_TYPE, 'Content-Length': 14 });
+    // res.write('{"ready":true}');
+    // res.end();
   });
   server.listen(port, '0.0.0.0', () => {
     console.log(`Example app listening at http://0.0.0.0:${port}`);
