4c5bdfd2270fab1a8eb35ea4c7b1ebbf107c7d75
Mo Kamioner <mkamioner@gmail.com>
2022-05-22 09:36:52 +0300

    more fixes

--- index.js
+++ index.js
@@ -2,39 +2,32 @@ const fs = require('fs');
 const net = require('net');
 const turboHttp = require('turbo-http');
 
-const PORT = +process.argv[2] || 3000;
 const PRIMARY_PORT = 4001;
-const cardsData = fs.readFileSync('./cards.json');
-const cards = JSON.parse(cardsData);
-const CARD_DATA = cards.map((card) => Buffer.from(JSON.stringify(card), 'ascii'));
-const COMPLETED_BODY = Buffer.from(JSON.stringify({ id: 'ALL CARDS' }), 'ascii');
+const PORT = +process.argv[2] || PRIMARY_PORT;
+const RAW_CARDS = JSON.parse(fs.readFileSync('./cards.json'));
+const CARD_DATA = RAW_CARDS.map((card) => Buffer.from(JSON.stringify(card), 'ascii'));
+CARD_DATA.unshift(Buffer.from(JSON.stringify({ id: 'ALL CARDS' }), 'ascii'));
 const USER_INDEXES = {};
+const CARD_COUNT = CARD_DATA.length;
 
 function createServer() {
-  const server = turboHttp.createServer((req, res) => {
-    const key = req.url.substring(13);
-    const index = (USER_INDEXES[key] ?? 100) - 1;
-    const body = CARD_DATA[index];
-    if (body) {
-      USER_INDEXES[key] = index;
-      res.end(body);
-    } else {
-      res.end(COMPLETED_BODY);
+  turboHttp.createServer((req, res) => {
+    const newIndex = (USER_INDEXES[req.url] ?? CARD_COUNT) - 1;
+    if (newIndex) {
+      USER_INDEXES[req.url] = newIndex;
     }
-  });
-
-  server.listen(PRIMARY_PORT, '0.0.0.0', () => {
+    res.end(CARD_DATA[newIndex]);
+  }).listen(PRIMARY_PORT, '0.0.0.0', () => {
     console.log('Main server alive');
   });
 }
 
 function mirrorServer() {
-  const mirror = net.createServer((soc) => {
+  net.createServer((soc) => {
     const target = net.createConnection({ port: PRIMARY_PORT, host: '0.0.0.0' });
     soc.pipe(target);
     target.pipe(soc);
-  });
-  mirror.listen(PORT, '0.0.0.0', () => {
+  }).listen(PORT, '0.0.0.0', () => {
     console.log('Mirror proxy server alive');
   });
 }
